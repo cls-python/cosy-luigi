f5cb46c5ba829b64375d33f53241bd5e462128a2
Constantin Chaumet <16308584+Jekannadar@users.noreply.github.com>
2026-03-30 11:45:36 +0200

    Update docs generation for examples, add additional example

---
+++ examples/lot_sizing/README.md
@@ -0,0 +1,28 @@
+# Lot Sizing Example
+
+You can find the source for the example
+[here](https://github.com/cls-python/cls-luigi/tree/main/examples/lot_sizing/):
+
+Here we utilized CLS-Luigi to construct demand prediction pipelines for
+lot sizing. The example was built to present CLS-Luigi at the
+[LION17](https://lion17.org/) conference.
+
+# Requirements
+
+The example contains a
+[requirements.txt](https://github.com/cls-python/cls-luigi/tree/main/examples/ny_taxi/requirements.txt)
+file. To experiment with the example, you can set up your environment by
+executing the following command:
+
+``` bash
+# cd into the lot sizing example folder
+pip install -r requirements.txt
+```
+
+# Static Visualization
+
+![image](images/static.png)
+
+# Dynamic Visualization
+
+![image](images/dynamic.png)
--- scripts/gen_example_pages.py
+++ scripts/gen_example_pages.py
@@ -1,5 +1,6 @@
 """Generate the examples pages and navigation."""
 
+import os
 import re
 from pathlib import Path
 
@@ -12,35 +13,64 @@ root = Path(__file__).parent.parent
 src = root / "examples"
 
 
-for path in sorted(src.rglob("*.py")):
-    module_path = path.relative_to(src).with_suffix("")
-    doc_path = path.relative_to(src).with_suffix(".md")
-    full_doc_path = Path("examples", doc_path)
+for directory in [Path(d.path) for d in os.scandir(src)]:
+    example_name = None
 
-    parts = tuple(module_path.parts)
-    with open(path) as f:
-        override_name = re.findall(r"##(.*?)##", f.readline())
+    for path in sorted(directory.rglob("*.md"), key=lambda s: "aaa" if s.name == "README.md" else s.name):
+        module_path = path.relative_to(src).with_suffix("")
+        doc_path = path.relative_to(src)
+        full_doc_path = Path("examples", doc_path)
 
-    if parts[-1] == "__init__":
-        parts = parts[:-1]
-    elif parts[-1] == "__main__":
-        continue
+        parts = tuple(module_path.parts)
 
-    if override_name:
-        nav[override_name] = doc_path.as_posix()
-    else:
-        nav[parts] = doc_path.as_posix()
+        with open(path) as f:
+            override_name = re.findall(r"^#\s*([^#].*)\s*$", f.readline())
 
-    with mkdocs_gen_files.open(full_doc_path, "w") as fd:
-        ident = ".".join(parts)
-        fd.write(
-            f"""::: {ident}
-        options:
-            members_order: source
-        """
-        )
+        if parts[-1] == "README":
+            example_name = tuple(override_name)
+            override_name = ("Overview",)
+        if example_name:
+            parts = example_name + parts[1:]
 
-    mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
+        if override_name:
+            nav[parts[:-1] + override_name] = doc_path.as_posix()
+        else:
+            nav[parts] = doc_path.as_posix()
+
+        with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+            fd.write(path.read_text())
+
+        mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
+
+    for path in sorted(directory.rglob("*.png")):
+        module_path = path.relative_to(src).with_suffix("")
+        doc_path = path.relative_to(src)
+        full_doc_path = Path("examples", doc_path)
+
+        with mkdocs_gen_files.open(full_doc_path, "wb") as fd:
+            fd.write(path.read_bytes())
+
+    for path in sorted(directory.rglob("*.py")):
+        module_path = path.relative_to(src).with_suffix("")
+        doc_path = path.relative_to(src).with_suffix(".md")
+        full_doc_path = Path("examples", doc_path)
+
+        parts = tuple(module_path.parts)
+        if example_name:
+            nav[(*example_name, "Source Files", *parts[1:-1], f"{parts[-1]}.py")] = doc_path.as_posix()
+        else:
+            nav[parts] = doc_path.as_posix()
+
+        with mkdocs_gen_files.open(full_doc_path, "w") as fd:
+            ident = ".".join(parts)
+            fd.write(
+                f"""::: {ident}
+            options:
+                members_order: source
+            """
+            )
+
+        mkdocs_gen_files.set_edit_path(full_doc_path, path.relative_to(root))
 
 with mkdocs_gen_files.open("examples/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
