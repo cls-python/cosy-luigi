147cc6379bf42b84908b61ac2a3d5cfc209b4340
Constantin Chaumet <16308584+Jekannadar@users.noreply.github.com>
2026-05-19 16:06:39 +0200

    Account for output file names in task ids (#11)
    * Account for output file names in task ids, which fixes caching
    
    * Reformat with ruff
    
    * Add test which highlights issue of "shading" and confirms it is fixed
    
    * Remove unneeded lines

--- tests/test_infinite_chain.py
+++ tests/test_infinite_chain.py
@@ -1,9 +1,13 @@
+import itertools
 from abc import ABC
 
 import pytest
 from cosy.maestro import Maestro
+from luigi.mock import MockTarget
 
 from cosy_luigi import CoSyLuigiRepo, CoSyLuigiTask, CoSyLuigiTaskParameter
+
+counter = itertools.count()
 
 
 class ChainLink(CoSyLuigiTask, ABC):
@@ -17,14 +21,13 @@ class StartingLink(ChainLink):
 class RepeatingLink(ChainLink):
     chain_link = CoSyLuigiTaskParameter(ChainLink)
 
-
-class FinalLink(CoSyLuigiTask):
-    chain_link = CoSyLuigiTaskParameter(ChainLink)
+    def output(self):
+        return {"counter": MockTarget(str(next(counter)))}
 
 
 @pytest.fixture
 def repo():
-    return CoSyLuigiRepo(ChainLink, FinalLink)
+    return CoSyLuigiRepo(ChainLink)
 
 
 def test_infinite_chain(repo):
@@ -32,14 +35,13 @@ def test_infinite_chain(repo):
         repo.cls_repo,
         repo.taxonomy,
     )
-    results = list(maestro.query(FinalLink.target(), max_count=10))
+    results = list(maestro.query(RepeatingLink.target(), max_count=10))
 
     # Check for shapes of the pipelines
     for i, result in enumerate(results):
         current_link = result
-        assert isinstance(current_link, FinalLink)
-        current_link = current_link.chain_link
         for _ in range(i):
             assert isinstance(current_link, RepeatingLink)
             current_link = current_link.chain_link
+        current_link = current_link.chain_link
         assert isinstance(current_link, StartingLink)
---
+++ tests/test_task_id_generation.py
@@ -0,0 +1,107 @@
+from abc import ABC
+
+import luigi
+import pytest
+from cosy.maestro import Maestro
+from luigi.mock import MockTarget
+from luigi.task import task_id_str
+
+from cosy_luigi import CoSyLuigiRepo, CoSyLuigiTask, CoSyLuigiTaskParameter
+
+target_a = MockTarget("A")
+target_b = MockTarget("B")
+fs = target_a.fs
+
+
+class Shaded(CoSyLuigiTask, ABC):
+    identifier: str
+
+
+class ShadedA(Shaded):
+    identifier = "A"
+
+
+class ShadedB(Shaded):
+    identifier = "B"
+
+
+class Shade(CoSyLuigiTask):
+    shaded = CoSyLuigiTaskParameter(Shaded)
+
+    def complete(self):
+        return True
+
+
+class Evaluate(CoSyLuigiTask):
+    shade = CoSyLuigiTaskParameter(Shade)
+
+    def run(self):
+        with self.output()["output"].open("w") as f:
+            f.write("OK.")
+
+    def output(self):
+        return {"output": MockTarget(self.shade.shaded.identifier)}
+
+
+class EvaluateWithPotentialToShade(Evaluate):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Copy the behaviour of regular Luigi
+        self.task_id = task_id_str(self.get_task_family(), self.to_str_params(only_significant=True, only_public=True))
+        self.__hash = hash(self.task_id)
+
+
+@pytest.fixture
+def repo():
+    return CoSyLuigiRepo(Evaluate, Shade, Shaded)
+
+
+@pytest.fixture
+def shadeable_repo():
+    return CoSyLuigiRepo(EvaluateWithPotentialToShade, Shade, Shaded)
+
+
+def test_shading_not_possible(repo):
+    fs.clear()
+    assert not target_a.exists()
+    assert not target_b.exists()
+    maestro = Maestro(
+        repo.cls_repo,
+        repo.taxonomy,
+    )
+    luigi.build(list(maestro.query(Evaluate.target())), local_scheduler=True, detailed_summary=True)
+    assert target_a.exists()
+    assert target_b.exists()
+
+
+def test_shading_would_be_possible(shadeable_repo):
+    fs.clear()
+    assert not target_a.exists()
+    assert not target_b.exists()
+    maestro = Maestro(
+        shadeable_repo.cls_repo,
+        shadeable_repo.taxonomy,
+    )
+    luigi.build(list(maestro.query(EvaluateWithPotentialToShade.target())), local_scheduler=True, detailed_summary=True)
+    assert not (target_a.exists() and target_b.exists())
+
+
+def test_output_mapping_is_enforced():
+    class TaskWithWrongOutputA(CoSyLuigiTask):
+        def output(self):
+            return MockTarget("")
+
+    with pytest.raises(TypeError):
+        TaskWithWrongOutputA()
+
+    class TaskWithWrongOutputB(CoSyLuigiTask):
+        def output(self):
+            return [MockTarget("")]
+
+    with pytest.raises(TypeError):
+        TaskWithWrongOutputB()
+
+    class TaskWithNoneOutput(CoSyLuigiTask):
+        pass
+
+    TaskWithNoneOutput()
